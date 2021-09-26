d875df6cc4e57ec2f0edec97662dcfd6ff1e6c00
Ma Tianqi <matianqi.tqma@bytedance.com>
2021-09-26 11:06:18 +0800

    doc(federation): update federation doc

---
+++ build/zh-cn/assets/js/3778fe86.ce1bd584.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{83:function(o){o.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"\u603b\u63fd","permalink":"/farrow-docs/build/zh-cn/blog/introduction"}]}')}}]);
